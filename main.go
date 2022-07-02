a7e453c010ca03d32e8da80fd72d14833a7ee9b6
PersesTitan <ToramPersesTitan@gmail.com>
2022-07-02 13:13:41 +0900

    임시 생략

--- main.go
+++ main.go
@@ -4,20 +4,19 @@ import (
 	"github.com/gin-gonic/gin"
 	"html/template"
 	"net/http"
-	"strings"
 )
 
 func main() {
 	router := gin.Default()
 
 	router.SetFuncMap(template.FuncMap{
-		"upper": strings.ToUpper,
-		"safe": func(s string) template.HTML {
-			return template.HTML(s)
-		},
+		//"upper": strings.ToUpper,
+		//"safe": func(s string) template.HTML {
+		//	return template.HTML(s)
+		//},
 	})
 
-	router.LoadHTMLFiles("gin_HTML/templates/about.html")
+	//router.LoadHTMLFiles("gin_HTML/templates/about.html")
 
 	//router.Static("/assets", "./assets")
 	router.LoadHTMLGlob("templates/*.html")
@@ -25,16 +24,17 @@ func main() {
 	router.GET("/", func(context *gin.Context) {
 		context.HTML(http.StatusOK, "index.html", gin.H{
 			"title":   "메인페이지 입니다.",
-			"message": "<a href='http://localhost:8080/about'>제목</a>",
+			"message": "제목",
+			//"url":     "/about",
 		})
 	})
 
-	router.GET("/about", func(context *gin.Context) {
-		context.HTML(http.StatusOK, "about.html", gin.H{
-			"about":   "서브 페이지",
-			"content": "안녕",
-		})
-	})
+	//router.GET("/about", func(context *gin.Context) {
+	//	context.HTML(http.StatusOK, "about.html", gin.H{
+	//		"about":   "서브 페이지",
+	//		"content": "안녕",
+	//	})
+	//})
 
 	//router := setupRouter()
 	_ = router.Run(":8080")
